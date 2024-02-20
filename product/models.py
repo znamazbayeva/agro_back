698e1c4e20225a406bf4872fb0f244388c3eb2e3
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2024-02-20 10:14:38 +0100

    fix(product): make the digitsinteger

--- product/models.py
+++ product/models.py
@@ -3,8 +3,8 @@ from category.models import Category, Subcategory
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    price = models.IntegerField(null=True, blank=True) 
+    discounted_price = models.IntegerField(null=True, blank=True) 
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
     manifacturor = models.CharField(max_length=200, null=True, blank=True)
     description = models.CharField(max_length=500, null=True, blank=True)
