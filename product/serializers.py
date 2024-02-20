f501404aa148fd35a90c35c7d2cab00559d08d6b
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2024-02-20 20:54:54 +0100

    fix(product): add category info

--- product/serializers.py
+++ product/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_info = serializers.SerializerMethodField()
+
+    def get_category_info(self, obj):
+        return {
+            'id': obj.category_info.id,
+            'name': obj.category_info.name,
+            # Include any other fields you want from the Category model
+        }
     class Meta:
         model = Product
         fields = '__all__'
