37bd2663041d55648848d936bf8ba92670f828f3
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2024-02-23 19:55:59 +0100

    fix(all): items

--- order/serializers.py
+++ order/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
 from product.models import Product
-from product.serializers import ProductSerializer
 from django.core.mail import EmailMessage
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,6 +21,8 @@ class OrderSerializer(serializers.ModelSerializer):
             OrderItem.objects.create(order_id=order.id, **item)
             product_data = Product.objects.get(id=item['product'].id)
             order_string += "Продукт: {} Количеcтво: {} Цена: {} \n".format(product_data.name, item['quantity'], product_data.price)
+            product_data.count -= item['quantity']
+            product_data.save()
         order_string += "Имя: {}, Телефон: {}".format(validated_data["name"], validated_data["phone"])
         email = EmailMessage(
             'Заказ #{}'.format(order.pk),
